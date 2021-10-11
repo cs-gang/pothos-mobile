71204ce7236599a7ed0e3d8706638ab0eeed9cc9
anand2312 <anandkrishna2312@gmail.com>
2021-10-11 23:09:42 +0530

    backend: make createNewTransaction function

--- src/services/data.ts
+++ src/services/data.ts
@@ -2,13 +2,23 @@ import firestore, {
   FirebaseFirestoreTypes,
 } from '@react-native-firebase/firestore';
 
+type TransactionType = 'income' | 'expenditure';
 interface UserData {
   username?: string;
+  currency: string;
+  tags: string[];
   transactions: FirebaseFirestoreTypes.CollectionReference;
 }
 
+interface Transaction {
+  transactionType: TransactionType;
+  amount: number;
+  name: string;
+  notes: string;
+  tags: string[];
+}
+
 export async function getUserData(uid: string): Promise<UserData | undefined> {
-  // Get a user's data. If the document doesn't exist in the database, create it.
   const docRef = firestore().collection('users').doc(uid);
   const userDoc = await docRef.get();
   const data = userDoc.data();
@@ -19,6 +29,29 @@ export async function getUserData(uid: string): Promise<UserData | undefined> {
         .collection('users')
         .doc(uid)
         .collection('transactions'),
+      currency: data.get('currency'),
+      tags: data.get('tags'),
     };
   }
+}
+
+export async function createNewTransaction(
+  uid: string,
+  transaction: Transaction,
+) {
+  // update the top-level user doc to include the tags
+  // arrayUnion will avoid repetition
+  let docRef = firestore().collection('users').doc(uid);
+  docRef.update({
+    tags: FirebaseFirestoreTypes.FieldValue.arrayUnion(...transaction.tags),
+  });
+  let transactionColRef = docRef.collection('transactions');
+  await transactionColRef.add({
+    name: transaction.name,
+    notes: transaction.notes,
+    amount: transaction.amount,
+    transactionOn: FirebaseFirestoreTypes.Timestamp.now(),
+    transactionType: transaction.transactionType,
+    tags: transaction.tags,
+  });
 }
