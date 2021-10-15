b6d737b9d30f6355939e10c54f17f2c776543c58
Windsmith <shriramsekar11@gmail.com>
2021-10-15 21:47:17 +0400

    feat: make slideable view for graph section

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, useWindowDimensions } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import tw from 'tailwind-react-native-classnames'
 
 export default function Dashboard() {
@@ -13,6 +14,35 @@ export default function Dashboard() {
         )
     }
 
+    const FirstRoute = () => (
+        <FlatList data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 5]} renderItem={(item) => <Item />} style={tw.style(" mt-3 mb-2")} />
+    );
+
+    const SecondRoute = () => (
+        <View style={tw.style("bg-green-600")} />
+    );
+
+    const renderScene = SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+    });
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        { key: 'first', title: 'Transactions' },
+        { key: 'second', title: 'Graphs' },
+    ]);
+
+    const layout = useWindowDimensions();
+
+    const renderTabBar = (props: any) => (
+        <TabBar
+            {...props}
+            indicatorStyle={{ backgroundColor: 'white' }}
+            style={tw.style("bg-green-600 mt-2")}
+        />
+    );
+
     return (
         <SafeAreaView style={tw.style("bg-green-600", { height: "100%", width: "100%" })}>
             <View style={tw.style("mx-auto mt-10 p-4 px-16 rounded-lg border-4 border-gray-800 bg-green-800 shadow-2xl")}>
@@ -21,7 +51,15 @@ export default function Dashboard() {
                 <Text style={tw.style("text-lg text-center text-green-400")}>Total Spent: 1000$</Text>
             </View>
 
-            <FlatList data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 5]} renderItem={(item) => <Item />} style={tw.style("mt-20 mb-2")} />
+            {/*<FlatList data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 5]} renderItem={(item) => <Item />} style={tw.style("mt-20 mb-2")} />*/}
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={{ width: layout.width }}
+                renderTabBar={renderTabBar}
+
+            />
         </SafeAreaView>
     )
 }
