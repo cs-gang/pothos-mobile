fca445c7715322790c9643ed998f8167ae46b279
Windsmith <shriramsekar11@gmail.com>
2021-10-15 16:28:53 +0400

    feat: make basic nav bar

--- src/screens/Transactions.tsx
+++ src/screens/Transactions.tsx
@@ -1,1 +1,14 @@
-export { }
+import React from 'react'
+import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'tailwind-react-native-classnames';
+
+export default function Transactions() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text style={tw.style("text-black")}>Transaction</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
